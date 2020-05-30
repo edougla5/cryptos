50ac7bb612d24bea317c958cb6a4b8e887303052
edougla5 <34756277+edougla5@users.noreply.github.com>
2020-05-30 19:20:24 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card opts
+    const cardArray = [
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        }
+        
+]
+
+const grid = document.querySelector('.grid');
+var cardsChosen = []
+
+//bord
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i),
+        card.addEventListener('click', flipCard)
+        //card.addEventListener('click', flipcard)
+        grid.appendChild(card)
+    }
+}
+
+//checking matches
+function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('found a match')
+        cards[optionOneId]
+    }}
+
+
+
+createBoard()
+})
